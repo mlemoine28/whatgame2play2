dfdf1cb9890cd483ef9063b447e03381880bdf31
mlemoine28 <145514725+mlemoine28@users.noreply.github.com>
2024-07-26 23:06:26 -0230

    changed color of Name: Michael to blue with Text component.

--- src/Components/Person/Person.jsx
+++ src/Components/Person/Person.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import Text from "../Text/Text";
 
 function Person({ name, age, colour}) {
  return (
      <div>
-         <h2>Name: {name}</h2>
+        <Text fontWeight="bold" color="blue">
+            <h2>Name: {name}</h2>
+        </Text>
          <h2>Age: {age}</h2>
          <h2>Colour: {colour}</h2>
      </div> 
--- src/Components/Text/Text.jsx
+++ src/Components/Text/Text.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
-function Text ({ children, size, fontWeight }) {
+function Text ({ children, size, fontWeight, color }) {
  return ( 
- <div style= {{ fontSize: size, fontWeight }}> 
+ <div style= {{ fontSize: size, fontWeight, color }}> 
  { children }
  </div>
  );
