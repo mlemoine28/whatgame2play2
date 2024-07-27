26beec4b3a2772c7839a250c2b5138ebc393bfe8
mlemoine28 <145514725+mlemoine28@users.noreply.github.com>
2024-07-26 23:45:19 -0230

    added style changes to person

--- src/Components/Person/Person.jsx
+++ src/Components/Person/Person.jsx
@@ -4,11 +4,11 @@ import Text from "../Text/Text";
 function Person({ name, age, colour}) {
  return (
      <div>
-        <Text fontWeight="bold" color="blue">
+        <Text fontWeight="bold" color="blue" size="50px">
             <h2>Name: {name}</h2>
         </Text>
-         <h2>Age: <Text color="red">{age}</Text></h2>
-         <h2>Colour: <Text color="yellow">{colour}</Text></h2>
+         <h2>Age: <Text color="red" fontWeight="bold" size="50px">{age}</Text></h2>
+         <h2>Colour: <Text color="yellow" fontWeight="bold" size="60px">{colour}</Text></h2>
      </div> 
  );
 }
