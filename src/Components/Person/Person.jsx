e0fb4cfd0e7b06b2b538f4dd3c6a2b714e977dde
mlemoine28 <145514725+mlemoine28@users.noreply.github.com>
2024-07-26 23:08:17 -0230

    Changed just "34" to red

--- src/Components/Person/Person.jsx
+++ src/Components/Person/Person.jsx
@@ -7,7 +7,7 @@ function Person({ name, age, colour}) {
         <Text fontWeight="bold" color="blue">
             <h2>Name: {name}</h2>
         </Text>
-         <h2>Age: {age}</h2>
+         <h2>Age: <Text color="red">{age}</Text></h2>
          <h2>Colour: {colour}</h2>
      </div> 
  );
