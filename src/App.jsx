fecb572ebd2576471f166b81a7c7c64f26d70908
mlemoine28 <145514725+mlemoine28@users.noreply.github.com>
2024-07-28 22:46:40 -0230

    Added a listgroup, testing bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,20 @@ import React from "react";
 import Person from "./Components/Person/Person.jsx";
 import Button from "./Components/Button/Button.jsx";
 import Text from "./Components/Text/Text.jsx";
+import ListGroup from "./Components/ListGroup/ListGroup.jsx";
 
 function App() {
-    const buttonClick = () => {
-        alert('Button clicked! Nice!');
-    };
-    
-    return (
-        <div>
-            <Person 
-                name="Michael" 
-                age={34} 
-                colour="Yellow"
-            />
-            <Button
-                onClick={buttonClick}
-                label={"Click me! Now!"}
-            />
-        </div>
-    );
+  const buttonClick = () => {
+    alert("Button clicked! Nice!");
+  };
+
+  return (
+    <div>
+      <Person name="Michael" age={34} colour="Yellow" />
+      <Button onClick={buttonClick} label={"Click me! Now!"} />
+      <ListGroup />
+    </div>
+  );
 }
-
-
 
 export default App; //not sure what the "default" part is though
