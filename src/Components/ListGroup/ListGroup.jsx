b45b270e6ef8a5464721a268703a13aa6b06125e
mlemoine28 <145514725+mlemoine28@users.noreply.github.com>
2024-07-29 22:44:25 -0230

    Updated list group to use map()

--- src/Components/ListGroup/ListGroup.jsx
+++ src/Components/ListGroup/ListGroup.jsx
@@ -1,20 +1,23 @@
 function ListGroup() {
   const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
 
-  items.map((item) => <li></li>);
-
   return (
     <div>
       {/*Instead of <div>, you can just do <> and </> if you want. */}
-      <h1>List</h1>
+      <h1>List of Places</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li
+            key={item}
+            className="list-group-item"
+            onClick={() => console.log("Clicked!")}
+          >
+            {item}
+          </li>
+        ))}
       </ul>
     </div>
   );
 }
+
 export default ListGroup;
