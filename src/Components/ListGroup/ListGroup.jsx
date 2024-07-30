a29b098c29664f85ec54b48e6a530de74b4646f3
mlemoine28 <145514725+mlemoine28@users.noreply.github.com>
2024-07-30 00:12:57 -0230

    continuing to update with useState

--- src/Components/ListGroup/ListGroup.jsx
+++ src/Components/ListGroup/ListGroup.jsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
+
 function ListGroup() {
   const items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+  useState; //Tells React that "this component has data or state that will change over time"
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <div>
       {/*Instead of <div>, you can just do <> and </> if you want. */}
       <h1>List of Places</h1>
       <ul className="list-group">
-        {items.map((item) => (
+        {items.map((item, index) => (
           <li
             key={item}
-            className="list-group-item"
-            onClick={() => console.log("Clicked!")}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => {
+              {
+                setSelectedIndex(index);
+              }
+            }}
           >
             {item}
           </li>
