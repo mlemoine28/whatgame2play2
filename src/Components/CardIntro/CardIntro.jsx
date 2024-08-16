505e0e1de51e00c0a2046e730844112a8ca1ed54
mlemoine28 <145514725+mlemoine28@users.noreply.github.com>
2024-08-16 14:36:26 -0230

    made handleButtonClick to hide the card after Get Started is clicked using useState

--- src/Components/CardIntro/CardIntro.jsx
+++ src/Components/CardIntro/CardIntro.jsx
@@ -8,24 +8,29 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Button2 from "../../Components/Button/Button2";
 
 function CardIntro() {
+  const [showCard, setShowCard] = useState(true);
+
+  const handleButtonClick = () => {
+    setShowCard(false);
+  };
+
   return (
     <div className="d-flex justify-content-center align-items-center vh-100 bg-dark">
-      <Card
-        className="bg-secondary border-success"
-        style={{ maxWidth: "60rem", height: "auto" }}
-      >
-        <Card.Img variant="top" src={GamingConsoles} />
-        <Card.Body className="bg-dark border border-success">
-          <Card.Title className="text-primary">Welcome!</Card.Title>
-          <Card.Text className="text-white">
-            Answer a few questions to find the best games for you.
-          </Card.Text>
-          <Button2
-            label="Get Started"
-            onClick={() => console.log("Clicked!")}
-          ></Button2>
-        </Card.Body>
-      </Card>
+      {showCard && ( // This means that if showCard is true, the card will be rendered.
+        <Card
+          className="bg-secondary border-success"
+          style={{ maxWidth: "60rem", height: "auto" }}
+        >
+          <Card.Img variant="top" src={GamingConsoles} />
+          <Card.Body className="bg-dark border border-success">
+            <Card.Title className="text-primary">Welcome!</Card.Title>
+            <Card.Text className="text-white">
+              Answer a few questions to find the best games for you.
+            </Card.Text>
+            <Button2 label="Get Started" onClick={handleButtonClick}></Button2>
+          </Card.Body>
+        </Card>
+      )}
     </div>
   );
 }
