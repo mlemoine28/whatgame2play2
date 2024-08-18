899c707278cab5c1bf238dbf8d37452a053de7b2
mlemoine28 <145514725+mlemoine28@users.noreply.github.com>
2024-08-17 23:02:03 -0230

    Updated main card

--- src/Components/MainCard/MainCard.jsx
+++ src/Components/MainCard/MainCard.jsx
@@ -6,8 +6,17 @@ import FormSubmit from "../../Components/FormSubmit/FormSubmit";
 import Form from "react-bootstrap/Form";
 
 function MainCard() {
+
+ const [showCard, setShowCard] = useState(true);
+
+ const handleButtonClick = () => {
+   setShowCard(false);
+ };
+
+
   return (
     <div className="d-flex justify-content-center align-items-center vh-100 bg-dark">
+      {showCard && (
       <Card
         className="bg-secondary border-success"
         style={{ maxWidth: "60rem", height: "auto" }}
@@ -19,8 +28,10 @@ function MainCard() {
             What genre of game are you looking to play?
           </Card.Text>
           <FormSubmit />
+          <Button2 label="Submit" onClick={handleButtonClick}></Button2>
         </Card.Body>
       </Card>
+       )}
     </div>
   );
 }
