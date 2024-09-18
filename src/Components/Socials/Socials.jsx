46a69186ddf8d2fc285e6b91e2e27c5c6d4df385
mlemoine28 <145514725+mlemoine28@users.noreply.github.com>
2024-09-17 22:36:42 -0230

    added socials icons

--- src/Components/Socials/Socials.jsx
+++ src/Components/Socials/Socials.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import linkedIn from "../../assets/linked_in.png";
 import styles from "./Socials.module.css";
+import githublogo from "../../assets/github-mark-white.svg"
 
 function Socials() {
   return (
     <div className={styles.socialstyle}>
       <p>Created by Michael LeMoine</p>
-      <a href="http://www.linkedin.com"> <img src={linkedIn} alt="linkedin"/></a>
-        
+
+      <a href="#" className={`fa fa-linkedin ${styles.fa}`}></a>
+      <img src={githublogo} className={styles.githubstyle}></img>
     </div>
   );
 }
