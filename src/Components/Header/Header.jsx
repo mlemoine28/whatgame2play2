d7fa75c1ab3e3696378efbd39cbe6792febe417d
mlemoine28 <145514725+mlemoine28@users.noreply.github.com>
2024-12-17 21:11:42 -0330

    Added click to home for WhatGame2Play image

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,12 +4,22 @@ import TitleImage from "../../assets/WhatGame2Play_Header.png";
 import styles from "./Header.module.css";
 import ButtonHome from "../Button/ButtonHome";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Header() {
+  const navigate = useNavigate();
+  const backToHomeClick = () => {
+    navigate("/whatgame2play2");
+  };
   return (
     <div className={styles.container}>
       <div className={styles.headerstyle}>
-        <img src={TitleImage} alt="WhatGame2Play" />
+        <img
+          src={TitleImage}
+          alt="WhatGame2Play"
+          onClick={backToHomeClick}
+          style={{ cursor: "pointer" }}
+        />
       </div>
     </div>
   );
