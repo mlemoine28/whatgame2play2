5b1b585ec512041d5d5d8d36cc58c2eac6ae9549
mlemoine28 <145514725+mlemoine28@users.noreply.github.com>
2024-12-18 22:04:09 -0330

    Most recent build

---
+++ src/Components/Pagination/Pagination.jsx
@@ -0,0 +1,2 @@
+// Make a reuseable pagination component, with stuff passed into it. Import react pagination in here. Set it up with ellipses, arrows, etc. Will have conditional rendering for when
+//ellipses will show. Have it so that page 1 is always visible.
