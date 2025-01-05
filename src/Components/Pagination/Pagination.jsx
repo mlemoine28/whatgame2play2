520732e5e6a0be64058e0f5559d56d73405c28a1
mlemoine28 <145514725+mlemoine28@users.noreply.github.com>
2025-01-05 16:16:58 -0330

    starting pagination component

--- src/Components/Pagination/Pagination.jsx
+++ src/Components/Pagination/Pagination.jsx
@@ -1,2 +1,5 @@
+import React from "react";
+import Pagination from "react-bootstrap/Pagination";
+
 // Make a reuseable pagination component, with stuff passed into it. Import react pagination in here. Set it up with ellipses, arrows, etc. Will have conditional rendering for when
 //ellipses will show. Have it so that page 1 is always visible.
