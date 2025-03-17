f7f7f440c08d6b2215c795f0afac5aa6bdeaac10
mlemoine28 <145514725+mlemoine28@users.noreply.github.com>
2025-03-16 23:48:33 -0230

    changed appearance of the input field

--- src/Components/PopupLogin/PopupLogin.jsx
+++ src/Components/PopupLogin/PopupLogin.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
-
+import Form from "react-bootstrap/Form";
 function PopupLogin() {
   const [show, setShow] = useState(false);
 
@@ -10,15 +10,39 @@ function PopupLogin() {
 
   return (
     <>
-      <Button variant="primary" onClick={handleShow}>
-        Launch demo modal
+      <Button variant="primary" onClick={handleShow} >
+        Login
       </Button>
 
       <Modal show={show} onHide={handleClose}>
         <Modal.Header closeButton>
-          <Modal.Title>Modal heading</Modal.Title>
+          <Modal.Title>Create an Account</Modal.Title>
         </Modal.Header>
-        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Form.Floating className="mb-3">
+          <Form.Control
+            id="floatingInputCustom"
+            type="email"
+            placeholder="name@example.com"
+          />
+          <label htmlFor="floatingInputCustom">Username</label>
+        </Form.Floating>
+        <Form.Floating className="mb-3">
+          <Form.Control
+            id="floatingInputCustom"
+            type="email"
+            placeholder="name@example.com"
+          />
+          <label htmlFor="floatingInputCustom">Email address</label>
+        </Form.Floating>
+        <Form.Floating>
+          <Form.Control
+            id="floatingPasswordCustom"
+            type="password"
+            placeholder="Password"
+          />
+          <label htmlFor="floatingPasswordCustom">Password</label>
+        </Form.Floating>
+
         <Modal.Footer>
           <Button variant="secondary" onClick={handleClose}>
             Close
