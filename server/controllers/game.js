d42a9802b9e604b30313ee1a292e6e187faa28cc
mlemoine28 <145514725+mlemoine28@users.noreply.github.com>
2025-06-04 23:15:58 -0230

    edited game table

--- server/controllers/game.js
+++ server/controllers/game.js
@@ -3,3 +3,40 @@ const cors = require("cors");
 const setUpConnection = require("../utils/setUpConnection.js"); // Import the database connection setup
 const app = express();
 app.use(cors()); // ALWAYS TAKE THESE FIRST 5 LINES WHENEVER YOU USE MYSQL TABLES. EVERY FILE IN THE CONTROLLERS FOLDER WILL BE A TABLE FROM MYSQL.
+
+const addGame = (req, res) => {
+ const con = setUpConnection();
+ 
+ let sql = `INSERT INTO game
+ (title,
+ description,
+ release_date,
+ metacritic_score,
+ cover_image_url,
+ website 
+ )
+ VALUES (?, ?, ?, ?, ?, ?)`; //These values will directly be taken from the table in workbench. Must match exactly what the columns are in the table.
+
+ const values = [
+   req.body.title,
+   req.body.description,
+   req.body.release_date,
+   req.body.metacritic_score,
+   req.body.cover_image_url,
+   req.body.website
+ ];
+
+ con.query(sql, values, (err, rows) => {
+ 
+   con.destroy();
+   if (!err) {
+     res.send(JSON.stringify(rows));
+   } else {
+     console.log("Error while performing Query.", err);
+   }
+ });
+};
+
+module.exports = {
+ addGame,
+};
